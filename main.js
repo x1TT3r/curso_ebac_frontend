2ae9eb48d70d373cf32203d09a885acb8998a585
x1TT3r <113714755+x1TT3r@users.noreply.github.com>
2022-10-07 19:01:46 -0300

    modified main js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 $('form').on('submit', function(e) {
     e.preventDefault();
@@ -14,21 +13,3 @@ $('form').on('submit', function(e) {
     $('#adicionar-nova-tarefa').val('');
     })
     })
-=======
-$(document).ready(function() {
-$('form').on('submit', function(e) {
-    e.preventDefault();
-    const addNovaTarefa = $('#adicionar-nova-tarefa').val();
-    const addListaDeTarefa = $('<li>' + addNovaTarefa + '</li>');
-    $(addNovaTarefa).appendTo($(addListaDeTarefa));
-    $(addListaDeTarefa).appendTo('ul');
-    $('#adicionar-nova-tarefa').val('');
-    $("li").click(function() {
-        if ($(this).css('text-decoration-line') == 'none')
-        $(this).css('text-decoration-line', 'line-through');
-        else
-        $(this).css('text-decoration-line', 'none');
-    })
-    })
-    })
->>>>>>> 7133e6cbea6ece689b42b201d608fe0b9b03757b
