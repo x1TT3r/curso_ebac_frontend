f607e8ff42e2223cf2b8fb907d7856505ba33072
x1TT3r <113714755+x1TT3r@users.noreply.github.com>
2022-10-07 19:33:01 -0300

    removido a condicional de remover o click

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-$('form').on('submit', function(e) {
-    e.preventDefault();
-    const addNovaTarefa = $('#adicionar-nova-tarefa').val();
-    const addListaDeTarefa = $('<li>' + addNovaTarefa + '</li>');
-    $(addListaDeTarefa).click(function() {
-        if ($(this).css('text-decoration-line') == 'none')
-        $(this).css('text-decoration-line', 'line-through');
-        else
-        $(this).css('text-decoration-line', 'none');
-    })
-    $(addListaDeTarefa).appendTo('ul');
-    $('#adicionar-nova-tarefa').val('');
-    })
-    })
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const addNovaTarefa = $('#adicionar-nova-tarefa').val();
+        const addListaDeTarefa = $('<li>' + addNovaTarefa + '</li>');
+        $(addListaDeTarefa).click(function() {
+            $(this).css('text-decoration-line', 'line-through');
+        })
+        $(addListaDeTarefa).appendTo('ul');
+        $('#adicionar-nova-tarefa').val('');
+        })
+        })
